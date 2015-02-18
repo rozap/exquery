5ac8bb14a69e87e7dcf113bd90281e6aff61b67a
Chris Duranti <chrisd1891@gmail.com>
2015-02-17 23:34:51 -0800

    an function

--- lib/exquery/query.ex
+++ lib/exquery/query.ex
@@ -1,3 +1,26 @@
 defmodule Exquery.Query do
-	
+  
+
+  defp matches?({_, _, attrs}, {:any, :any, kvs}) do
+    Enum.all?(kvs, fn kv -> Enum.member?(attrs, kv) end)
+  end
+  defp matches?({el, children}, kv), do: matches?(el, kv)
+
+  defp children_of({_, _, _}), do: []
+  defp children_of({el, children}), do: children 
+
+
+  def one([], _), do: nil
+  def one(tree, {kind, contents, kv} = spec) do
+    el = Enum.find(tree, false, fn el -> matches?(el, spec) end)
+    if !el do
+      tree
+      |> Enum.map(&(children_of &1))
+      |> List.flatten
+      |> one(spec)
+    else
+      el
+    end
+  end
+
 end
--- test/query_test.exs
+++ test/query_test.exs
@@ -5,16 +5,31 @@ defmodule QueryTest do
   import ExqueryTest.Helpers
   import ExUnit.CaptureIO
 
-
-  test "can query by class" do
+  setup_all do
     tree = "no_js_css"
     |> fixture
     |> Exquery.tree
 
-
-    |> Tree.print
-
+    {:ok, %{tree: tree}}
   end
+
+  test "can find one element by attrs", %{tree: tree} do
+    assert Q.one(tree, {:any, :any, [{"id", "siteNotice"}]}) == {
+      {:tag, "div", [{"id", "siteNotice"}]}, 
+      [{:comment, " CentralNotice ", []}]
+    }
+
+    Q.one(tree, {:any, :any, [{"class", "noprint"}]}) == {
+      {:tag, "div", [{"id", "mw-page-base"}, {"class", "noprint"}]}
+    }
+
+    assert Q.one(tree, {:any, :any, [{"id", "siteNotice", "class", "foobar"}]}) == nil
+  end
+
+
+
+
+
 
 
 end
