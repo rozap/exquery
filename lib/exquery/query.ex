751e277de9c27c1ea92a3098aa62df93518a0fc3
Chris Duranti <chrisd1891@gmail.com>
2015-02-19 17:27:15 -0800

    stuff

--- lib/exquery/query.ex
+++ lib/exquery/query.ex
@@ -1,13 +1,42 @@
 defmodule Exquery.Query do
-  
+  import Exquery.Helpers
 
   defp matches?({_, _, attrs}, {:any, :any, kvs}) do
     Enum.all?(kvs, fn kv -> Enum.member?(attrs, kv) end)
+  end  
+  defp matches?({tag, _, attrs} = el, {tag, contents, kvs}) when tag != :any do
+    matches?(el, {:any, contents, kvs})
   end
+  defp matches?({_, contents, attrs} = el, {tag, contents, kvs}) when contents != :any do
+    matches?(el, {tag, :any, kvs})
+  end
+
+  defp matches?({_, _, _}, {_, _, _}), do: false
+
   defp matches?({el, children}, kv), do: matches?(el, kv)
+
 
   defp children_of({_, _, _}), do: []
   defp children_of({el, children}), do: children 
+
+
+
+  def all([], _, acc), do: Enum.reverse(acc)
+  def all(tree, {kind, contents, kv} = spec, acc) do
+    new_acc = Enum.reduce(tree, acc, fn(el, a) ->
+      if matches?(el, spec) do
+        [el | a]
+      else
+        a
+      end
+    end)
+
+    tree
+    |> Enum.map(&(children_of &1))
+    |> List.flatten
+    |> all(spec, new_acc)
+  end
+  def all(tree, spec), do: all(tree, spec, [])
 
 
   def one([], _), do: nil
@@ -16,8 +45,7 @@ defmodule Exquery.Query do
     if !el do
       tree
       |> Enum.map(&(children_of &1))
-      |> List.flatten
-      |> one(spec)
+      |> Enum.find_value(fn children -> one(children, spec) end)
     else
       el
     end
--- test/query_test.exs
+++ test/query_test.exs
@@ -1,16 +1,19 @@
 defmodule QueryTest do
   use ExUnit.Case
-  alias Exquery.Tree, as: Tree
   alias Exquery.Query, as: Q
   import ExqueryTest.Helpers
-  import ExUnit.CaptureIO
 
   setup_all do
     tree = "no_js_css"
     |> fixture
     |> Exquery.tree
 
-    {:ok, %{tree: tree}}
+    simple = "simple"
+    |> fixture
+    |> Exquery.tree
+
+
+    {:ok, %{tree: tree, simple: simple}}
   end
 
   test "can find one element by attrs", %{tree: tree} do
@@ -19,14 +22,51 @@ defmodule QueryTest do
       [{:comment, " CentralNotice ", []}]
     }
 
-    Q.one(tree, {:any, :any, [{"class", "noprint"}]}) == {
-      {:tag, "div", [{"id", "mw-page-base"}, {"class", "noprint"}]}
+    assert Q.one(tree, {:any, :any, [{"class", "noprint"}]}) == {
+      {:tag, "div", [{"class", "noprint"}, {"id", "mw-page-base"}]}, []
     }
 
     assert Q.one(tree, {:any, :any, [{"id", "siteNotice", "class", "foobar"}]}) == nil
   end
 
+  test "can find all elements by attrs", %{simple: simple} do
+    [one, two, three] = Q.all(simple, {:any, :any, [{"class", "noprint"}]})
 
+
+    assert one == {{:tag, "div", [{"class", "noprint"}, {"id", "mw-page-base"}]}, []}
+    assert two == {{:tag, "div", [{"class", "noprint"}, {"id", "mw-head-base"}]}, []}
+
+    {three, _} = three
+    assert three == {:tag, "ul", [{"class", "noprint"}, {"id", "footer-icons"}]}
+  end
+
+  test "can find the first element by tag", %{tree: tree} do
+    h1 = Q.one(tree, {:tag, "h1", []})
+    assert {{:tag, "h1", [
+      {"lang", "en"}, 
+      {"class", "firstHeading"}, 
+      {"id", "firstHeading"}]},
+      
+      [{:text, "Elixir (programming language)", []}]
+    } == h1
+  end
+
+  test "can find the first element by tag", %{tree: tree} do
+    h1 = Q.one(tree, {:tag, "h1", []})
+    assert {{:tag, "h1", [
+      {"lang", "en"}, 
+      {"class", "firstHeading"}, 
+      {"id", "firstHeading"}]},
+      
+      [{:text, "Elixir (programming language)", []}]
+    } == h1
+  end
+
+
+  test "can find element by tag and attrs", %{tree: tree} do
+    div = Q.one(tree, {:tag, "div", [{"style", "clear:both"}]})
+    assert div == {{:tag, "div", [{"style", "clear:both"}]}, []}
+  end
 
 
 
