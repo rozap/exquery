7c4ea3192796d334f97c6a9bbfb613c15ddd7e41
Chris Duranti <chrisd1891@gmail.com>
2015-02-21 10:54:34 -0800

    mixfile metadata

--- mix.exs
+++ mix.exs
@@ -3,27 +3,34 @@ defmodule Exquery.Mixfile do
 
   def project do
     [app: :exquery,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.0",
+     description: description,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
+  defp description do
+    """
+      A library for parsing HTML and querying elements within. Handy for web scraping or autmated testing.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "README.md", "config", "LICENSE", "mix.exs"],
+      contributors: ["Chris Duranti"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/rozap/exquery"
+      }
+    ]
+  end
+
   defp deps do
     []
   end
